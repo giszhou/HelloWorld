9361f630bf76db108c9b674bd69d5a625370e661
JZ <122569479+gisjz@users.noreply.github.com>
2024-01-18 13:37:18 -0600

    Update Program.cs
    remove enjoy C# 13

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!!");
-        Console.WriteLine("Welcome to .NET 8.0 World!");
-        Console.WriteLine("Enjoy C# 13 World!");
+        Console.WriteLine("Welcome to .NET 8.0 World!");        
     }
 }
