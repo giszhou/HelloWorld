d824e3f5849af9f8e270cbed7add57ee974b608f
giszhou <gis.zhou@hotmail.com>
2024-01-18 14:58:59 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,5 +8,6 @@ class Program
         Console.WriteLine("Welcome to .NET 8.0 World!");
         Console.WriteLine("Enjoy C# 13 World!");
         Console.WriteLine("Enjoy EF 8.0 World!");
+        Console.WriteLine("Welcome to ODATA v4 World!");
     }
 }
